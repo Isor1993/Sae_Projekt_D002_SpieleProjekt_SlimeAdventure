c19a3b8fb014534923fa803e31c44f15d048df83
Isor <eric.rosenberg@web.de>
2026-02-09 21:40:31 +0100

    SkyController Fix Update

--- Assets/Scripts/Systems/SkyManager/SkyController.cs
+++ Assets/Scripts/Systems/SkyManager/SkyController.cs
@@ -7,11 +7,16 @@ public class SkyController : MonoBehaviour
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private Transform _mainCameraT;
 
-    [SerializeField] private float _spawnOffset = 5f;
-    [SerializeField] private float _despawnOffset = 5f;
-    
-    private float _camLeftBoarder;
-    private float _camRightBoarder;
+    [SerializeField] private float _spawnOffset_1 = 5f;
+
+    [SerializeField] private float _spawnOffset_2 = 5f;
+
+    [SerializeField] private float _despawnOffset_1 = 7f;
+
+    [SerializeField] private float _despawnOffset_2 = 7f;
+
+
+
     private float _despawnPos_1;
     private float _despawnPos_2;
     private float _spawnPos_1;
@@ -21,9 +26,8 @@ public class SkyController : MonoBehaviour
 
 
     private Clouds[] _cloudArray;
-    private float _camMovement;
-    private Vector3 _currentCamPos;
-    private Vector3 _lastCameraPos;
+    private Vector3 _lastCamPos;
+    private Vector3 _camDelta;
 
     private void Awake()
     {
@@ -35,54 +39,55 @@ public class SkyController : MonoBehaviour
     }
     private void Start()
     {
-        _lastCameraPos = _mainCameraT.position;
+        _lastCamPos = _mainCameraT.position;
     }
 
     private void Update()
     {
-        float camX=_mainCameraT.position.x;
-        var camHalfWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
-        
-        _spawnPos_1 = camX + camHalfWidth + _spawnOffset;   // Spawn rechts
-        _despawnPos_1 = camX - camHalfWidth - _despawnOffset; // Despawn links
 
-        _spawnPos_2 = camX - camHalfWidth - _spawnOffset;  // Spawn links
-        _despawnPos_2 = camX + camHalfWidth + _despawnOffset; // Despawn rechts
+        var currentCamPos = _mainCameraT.position;
+        _camDelta = currentCamPos - _lastCamPos;
 
-        Debug.LogWarning(_camLeftBoarder);
-        Debug.LogWarning(_camRightBoarder);
+        DefineCloudBounds(currentCamPos);
 
-
-
-        GetCamerMovement();
         foreach (var cloud in _cloudArray)
         {
-            
-            var cloudMovement = (_camMovement * cloud.ParallaxFaktor) + (cloud.CloudMovement * Time.deltaTime);
-            cloud.transform.position += new Vector3(cloudMovement, 0, 0);
-
-            if (cloud.transform.position.x < _despawnPos_1)
-            {
-                cloud.transform.position = new Vector3(_spawnPos_1, cloud.transform.position.y, cloud.transform.position.z);
-            }
-            else if (cloud.transform.position.x > _despawnPos_2)
-            {
-                cloud.transform.position = new Vector3(_spawnPos_2, cloud.transform.position.y, cloud.transform.position.z);
-
-            }
+            ApplyCloudMovement(cloud);
+            HandleCloudRecycling(cloud);
         }
+        _lastCamPos = currentCamPos;
+    }
+    
+    private void ApplyCloudMovement(Clouds cloud)
+    {
+        var parallaxMovement = -_camDelta * cloud.ParallaxFaktor;
+        var cloudDrift = Vector3.right * cloud.CloudMovement * Time.deltaTime;
 
+        cloud.transform.position += new Vector3(parallaxMovement.x + cloudDrift.x, 0f, 0f);
     }
 
-    /// <summary>S
-    /// 
-    /// </summary>
-    private void GetCamerMovement()
+    private void DefineCloudBounds(Vector3 currentCamPos)
     {
-        _currentCamPos = _mainCameraT.position;
-        var camDiff = _currentCamPos.x - _lastCameraPos.x;
-        _camMovement = camDiff;
-        _lastCameraPos = _currentCamPos;
+        var camHalfWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
+
+        _spawnPos_1 = currentCamPos.x + camHalfWidth + _spawnOffset_1;
+        _despawnPos_1 = currentCamPos.x - camHalfWidth - _despawnOffset_1;
+
+        _spawnPos_2 = currentCamPos.x - camHalfWidth - _spawnOffset_2;
+        _despawnPos_2 = currentCamPos.x + camHalfWidth + _despawnOffset_2;
+    }
+
+    private void HandleCloudRecycling(Clouds cloud)
+    {
+        if (cloud.transform.position.x < _despawnPos_1)
+        {
+            cloud.transform.position = new Vector3(_spawnPos_1, cloud.transform.position.y, cloud.transform.position.z);
+        }
+        else if (cloud.transform.position.x > _despawnPos_2)
+        {
+            cloud.transform.position = new Vector3(_spawnPos_2, cloud.transform.position.y, cloud.transform.position.z);
+
+        }
     }
 
     private void OnDrawGizmos()
@@ -92,7 +97,7 @@ public class SkyController : MonoBehaviour
         Gizmos.DrawWireCube(new Vector3(_despawnPos_1, 5f), new Vector3(1f, 1f, 1f));
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(new Vector3(_spawnPos_2, 5f), new Vector3(1f, 1f, 1f));
-        Gizmos.DrawWireCube(new Vector3(_despawnPos_2, 5f), new Vector3(1f, 1f, 1f));
+        Gizmos.DrawWireCube(new Vector3(_spawnPos_2, 2f), new Vector3(1f, 1f, 1f));
+        Gizmos.DrawWireCube(new Vector3(_despawnPos_2, 2f), new Vector3(1f, 1f, 1f));
     }
 }
