7286dd48a5aae449ad519b63d24881e31e042742
Isor <eric.rosenberg@web.de>
2026-02-10 22:40:14 +0100

    Added Animation to coins

--- Assets/Scripts/Enviroment/Item_Coin.cs
+++ Assets/Scripts/Enviroment/Item_Coin.cs
@@ -2,14 +2,28 @@ using UnityEngine;
 
 public class Item_Coin : MonoBehaviour, ICollectable
 {
-
     
+    [SerializeField] private Vector2 _startPosition;
+    [SerializeField] private Vector2 _stepPos_1;   
+    [SerializeField] private Vector2 _endPosition;
+    [SerializeField] private float _duration_1;
+    [SerializeField] private float _duration_2;
+    [SerializeField] private float _duration_3;
+    [SerializeField] private float _duration_4;
+    private float _t;
+
+
     public void Collect(Player_Inventory inventory)
     {
         inventory.AddCoin(1);
         this.gameObject.SetActive(false);
     }
 
+    private void Awake()
+    {
+        _startPosition = transform.position;
+       
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,6 +33,18 @@ public class Item_Coin : MonoBehaviour, ICollectable
     // Update is called once per frame
     void Update()
     {
+        _t = Time.time*_duration_1;
+        
+        transform.position = MoveCoin(_startPosition, _endPosition, _t);
+       
 
+
+    }
+
+    private Vector2 MoveCoin(Vector2 start,Vector2 end,float t)
+    {
+        var nextPosition=Vector2.Lerp(start, end, t);      
+        return nextPosition;
+        
     }
 }
